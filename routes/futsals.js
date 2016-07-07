8acba4d3ceafb101a134003dd2397cb25d300b29
Nishan Bajracharya <nishan_cd@ymail.com>
2016-07-07 12:31:30 +0545

    Update Script Added

--- routes/futsals.js
+++ routes/futsals.js
@@ -24,6 +24,42 @@ router.route('/:name').get(function(req,res){
 	})
 })
 
+router.route('/:name/edit').get(function(req,res){
+	var name = req.params.name;
+	Futsal.findOne({name:name},function(err,data){
+		if(err) throw err;
+		//console.log(data);
+		res.render("edit",{title:"Futsals In Nepal", results:data})
+	})
+})
+
+router.route('/:name/edit').post(function(req,res){
+	var name = req.params.name;
+	var nameNew = req.body.name;
+	var address = req.body.address;
+	var phone = Number(req.body.phone);
+	var rate = Number(req.body.rate);
+	var openhours = req.body.openhours;
+	//console.log(rate);
+	//console.log(Futsal);
+	Futsal.findOneAndUpdate({name:name},
+	{
+		name: nameNew,
+		address: address,
+		phone: phone,
+		rate: rate,
+		openhours: openhours
+	},
+	function(err,data){
+		if(err) {
+			console.log("!!!!!!!#################Error");
+			throw err;
+		}
+		console.log("Updated");
+	})
+	return res.redirect("/");
+})
+
 router.route('/add').post(function(req,res){
 	var name = req.body.name;
 	var address = req.body.address;
